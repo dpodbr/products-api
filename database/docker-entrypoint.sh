6640b641d93e91f0a6b4732fdb9c3e25862e270a
David Podbregar <david.podbregar@gmail.com>
2025-08-13 14:27:59 +0200

    Added .editorconfig for a consistent formatting style. Added database (with pgadmin) docker configuration.

---
+++ database/docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+cat > /var/lib/pgadmin/pgpass <<EOF
+${DB_HOST}:${DB_PORT}:*:${DB_USERNAME}:${DB_PASSWORD}
+EOF
+chmod 600 /var/lib/pgadmin/pgpass
+
+cat > /var/lib/pgadmin/servers.json <<EOF
+{
+  "Servers": {
+    "1": {
+      "Name": "Local Postgres",
+      "Group": "Servers",
+      "Host": "${DB_HOST}",
+      "Port": ${DB_PORT},
+      "MaintenanceDB": "postgres",
+      "Username": "${DB_USERNAME}",
+      "SSLMode": "prefer",
+      "PassFile": "/var/lib/pgadmin/pgpass"
+    }
+  }
+}
+EOF
+chmod 600 /var/lib/pgadmin/servers.json
+
+# Chain to the original entrypoint of the base image
+exec /entrypoint.sh
