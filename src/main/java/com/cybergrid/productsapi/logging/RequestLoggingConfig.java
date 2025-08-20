3c32458450ad1e83d9727680a3d3ac9f1585b474
David Podbregar <david.podbregar@gmail.com>
2025-08-20 16:56:51 +0200

    Added basic request logging.

---
+++ src/main/java/com/cybergrid/productsapi/logging/RequestLoggingConfig.java
@@ -0,0 +1,20 @@
+package com.cybergrid.productsapi.logging;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingConfig {
+
+  @Bean
+  public CommonsRequestLoggingFilter requestLoggingFilter() {
+    CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
+    filter.setIncludeClientInfo(true);
+    filter.setIncludeQueryString(true);
+    filter.setIncludeHeaders(false);
+    filter.setIncludePayload(true);
+    filter.setMaxPayloadLength(2000);
+    return filter;
+  }
+}
